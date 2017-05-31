224872046ec6ce584ffd9ae9e4a8c667fa7ee819
HMSanchez <hmsanchez98@gmail.com>
2017-05-31 16:09:51 -0600

    Create Board.cs

---
+++ Board.cs
@@ -0,0 +1,18 @@
+using Battleship.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship.Models.Board
+{
+   public class Board
+    {
+        private TileStates[,] BattleBoard;
+        public void CheckBoard(TileStates board)
+        {
+
+        }
+    }
+}
