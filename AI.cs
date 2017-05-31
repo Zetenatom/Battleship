d787057c0286ea648b3acc3dc59c4f9722d13ba1
HMSanchez <hmsanchez98@gmail.com>
2017-05-31 15:28:34 -0600

    Create AI.cs

---
+++ AI.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship.Models.Players
+{
+    public class AI : Player
+    {
+        public AI(string name) : base(name)
+        {
+
+        }
+    }
+}
