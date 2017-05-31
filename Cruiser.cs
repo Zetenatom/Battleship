69d94c1dcabd3d70a9d171cd9bad7e28c4b67751
HMSanchez <hmsanchez98@gmail.com>
2017-05-31 15:48:50 -0600

    Create Cruiser.cs

---
+++ Cruiser.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship.Models.Ships
+{
+    public class Cruiser : Ship
+    {
+        public Cruiser(string name, int size) : base(name, size)
+        {
+
+        }
+    }
+}
